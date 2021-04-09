21a6690c0110bb6fa65841ac3042febcc1667029
Fatinabimanyu <79156679+Fatinabimanyu@users.noreply.github.com>
2021-04-10 06:40:33 +0700

    Add files via upload

---
+++ UTSPBOFatin/Kecil.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+namespace PesenMinum
+{
+    class Kecil : Drink
+    {
+        public Kecil()
+        {
+            this.price += 2000;
+        }
+    }
+}
---
+++ UTSPBOFatin/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AplikasiJajan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
